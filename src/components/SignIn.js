94b99ca5aadebb18d6f8e7c901bade86575d2238
TAIP25 <bouleon25@gmail.com>
2023-06-05 00:48:19 +0200

    Code final v4

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -75,8 +75,8 @@ export default function SignUp({ snackbar, setSnackbar }) {
             document.cookie = "authcookie=; path=/; max-age=-1";
             document.cookie = "isAdmin=; path=/; max-age=-1";
             if(response.data.severity === "success"){
-                document.cookie = `authcookie=${response.data.token}; path=/; max-age=${60*60*24*7}; samesite=lax; secure;`
-                document.cookie = `isAdmin=${response.data.admin}; path=/; max-age=${60*60*24*7}; samesite=lax; secure;`;
+                document.cookie = `authcookie=${response.data.token}; path=/; max-age=${60*60*24*7}; samesite=none; secure;`;
+                document.cookie = `isAdmin=${response.data.admin}; path=/; max-age=${60*60*24*7}; samesite=none; secure;`;
             }
             // On ajoute un snackbar pour indiquer à l'utilisateur la réponse du serveur
             setSnackbar({
